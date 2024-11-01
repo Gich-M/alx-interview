32f8c5c6a2b177e0eed5517d239394ad9f3b414f
Gich-M <gichmuriuki21@gmail.com>
2024-11-01 18:02:27 +0300

    UTF-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Module to determine a valid UTF-8 encoding."""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+
+    Args:
+        data: A list of integers representing bytes.
+
+    Return:
+        bool: True if the data is valid UTF-8, else False.
+    """
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes > 0:
+            if not (byte >> 6 == 0b10):
+                return False
+            num_bytes -= 1
+
+        elif byte >> 7 == 0:
+            continue
+        elif byte >> 5 == 0b110:
+            num_bytes = 1
+
+        elif byte >> 4 == 0b1110:
+            num_bytes = 2
+
+        elif byte >> 3 == 0b11110:
+            num_bytes = 3
+
+        else:
+            return False
+    return num_bytes == 0
